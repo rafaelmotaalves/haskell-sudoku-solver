00ea095d5d93935e9628ace4c138f2f07cbc2473
Rafael Mota <rma7@cin.ufpe.br>
2021-04-24 20:46:01 -0300

    Optimize sudoku algorithm

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,7 +1,9 @@
 module Sudoku where
 
 import Data.Array
+import Data.List
 import Data.List.Split
+import Control.Monad
 
 type Board = Array Int Int
 
@@ -37,15 +39,26 @@ checkSquare brd x y val = val `notElem` [ get brd i j | i <- [base_x..base_x + 2
 isValid :: Board -> Bool
 isValid brd = all (\(x, y) -> let val = (get brd x y) in if (val /= 0) then (possible (set brd x y 0) x y val) else True) [ (x, y) | x <- [0..8], y <- [0..8]]
 
-isSolved :: Board -> Bool
-isSolved brd = 0 `notElem` (elems brd)
-
 solve :: Board -> Maybe Board
-solve brd
-  | isSolved brd = Just brd
-  | otherwise = foldl (trySolve brd) Nothing
-    [(i, j, z) | i <- [0..8], j <- [0..8], get brd i j == 0, z <- [1..9], possible brd i j z] 
+solve brd = solve' brd emptys
+  where emptys = [ (x, y) | x <- [0..8], y <- [0..8], get brd x y == 0]
 
-trySolve :: Board -> Maybe Board -> (Int, Int, Int) -> Maybe Board
-trySolve brd Nothing (x, y, z) = solve (set brd x y z)
-trySolve _ (Just b) _ = Just b
+candidates :: Board -> Int -> Int -> [Int]
+candidates brd x y = [1..9] \\ (row++col++box)
+  where
+    row = rowValues brd x 
+    col = colValues brd y
+    box = boxValues brd x y 
+
+rowValues brd x = [ v | i <- [0..8], let v = get brd x i, v /= 0]
+colValues brd y = [ v | i <- [0..8], let v = get brd i y, v /= 0]
+
+boxValues brd x y = [ v | i <- [base_x..base_x + 2], j <- [base_y..base_y + 2], let v = get brd i j, v /= 0]
+  where base_x = (x `div` 3) * 3
+        base_y = (y `div` 3) * 3
+
+solve' :: Board -> [(Int, Int)] -> Maybe Board
+solve' brd [] = Just brd
+solve' brd ((i,j):xs) = case (candidates brd i j) of
+  [] -> Nothing
+  cands -> msum [ solve' (set brd i j c) xs | c <- cands ]
