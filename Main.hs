948e6046d43a2f2fc53786043cb45a0f1c765b5f
Rafael Mota <rma7@cin.ufpe.br>
2021-04-26 18:53:29 -0300

    Refactor main

--- Main.hs
+++ Main.hs
@@ -7,22 +7,21 @@ import Sudoku
 main = do
     [filename] <- getArgs
     input <- readInput filename
-    if (inputValidSize input) then
+    if inputValidSize input then
         let brd = makeBoard input in
-            if (isValid brd) then
-                let result = solve (makeBoard input) in
-                    putStrLn (formatOutput result)
-            else print "Invalid board: this sudoku is already invalid"
-    else print "Invalid board: wrong size"
+            if isValid brd then
+                let result = solve brd in putStrLn (formatOutput result)
+            else print "Invalid board: this sudoku is already invalid."
+    else print "Invalid board: wrong size, expected a 9x9 board."
 
 formatOutput :: Maybe Board -> String
-formatOutput Nothing = "This board has no solution"
+formatOutput Nothing = "Couldn't find a solution for the board."
 formatOutput (Just brd) = showSolution brd
 
 showSolution :: Board -> String
-showSolution brd = unlines (map join (boardToList brd))
+showSolution brd = unlines (map joinRow (boardToList brd))
 
-join :: [Int] -> String
-join [] = " "
-join [x]  = show x
-join (x:xs) = show x ++ " " ++ join xs
+joinRow :: [Int] -> String
+joinRow [] = ""
+joinRow [x]  = show x
+joinRow (x:xs) = show x ++ " " ++ joinRow xs
